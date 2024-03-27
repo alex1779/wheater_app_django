37ea9fcd3c341514e2c74a4c7183aa42631ab1c6
alex1779 <alex1779@gmail.com>
2024-03-27 14:22:00 -0300

    update

--- weather/urls.py
+++ weather/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from weather import views
 
 urlpatterns = [
-    path('', views.Home, name='Home'),
+    path('', views.index, name='index'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+import requests
 
-def Home(request):
-    
-    return render(request, 'weather/weather.html')
+def index(request):
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=66ad99905eeff16da429576520a894d7'
+
+    city = 'Madrid'
+
+    r = requests.get(url.format(city)).json()
+
+    name = r['name']
+    main = r['weather'][0]['main']
+    description = r['weather'][0]['description']
+    icon = r['weather'][0]['icon']
+    temp = str(r['main']['temp']) + ' F'
+
+    context = {'name':name, 'main':main, 'description':description, 'icon':icon, 'temp':temp,}
+    print(context)
+
+    return render(request, 'weather/weather.html', context)
